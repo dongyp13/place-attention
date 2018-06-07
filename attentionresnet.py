f4e7b9d34d862cbd5189916a20101f3f99d5c843
dongyp13 <dongyinpeng@gmail.com>
2018-06-07 16:08:08 +0800

    a

--- attentionresnet.py
+++ attentionresnet.py
@@ -177,18 +177,18 @@ class AttnPool(nn.Module):
 
     def forward(self, x):
         h = self.att1(x)
-        g = self.conv(h)
+        g = self.conv(x)
         g = self.bn(g)
         g = self.relu(g)
         g = self.pool(g)
         g = g.view(g.size(0), -1)
 
-        #permute_x = x.resize(x.size(0), x.size(1), x.size(2)*x.size(3)).permute(0,2,1)
+        permute_x = x.resize(x.size(0), x.size(1), x.size(2)*x.size(3)).permute(0,2,1)
         permute_h = h.resize(h.size(0), h.size(1), h.size(2)*h.size(3)).permute(0,2,1)
         attn_weight = nn.functional.softmax(
             self.trans(permute_h + g.unsqueeze(1)).squeeze(),
             dim=1)
-        out = torch.matmul(attn_weight.unsqueeze(1), permute_h).squeeze()
+        out = torch.matmul(attn_weight.unsqueeze(1), permute_x).squeeze()
         return out
         
 
